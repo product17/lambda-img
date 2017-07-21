b9539aee39ebbf9ec9025d91a348154949d614cf
Brent Crocker <brent.crocker@concur.com>
2017-07-21 15:43:17 -0700

    inital commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// import sharp from 'sharp';
+const sharp = require('sharp');
+
+// sharp('~/Sites/sample-images/sample-water.jpg')
+// sharp('~/Sites/sample-images/bird_sample.jpg')
+//   .resize(200)
+//   .toFile('~/Sites/sample-images/output.jpg', (err, info) => {
+//     console.log(err, info);
+//   });
+
+console.log(sharp)
+
+// .then( data => ... )
+// .catch( err => ... );
